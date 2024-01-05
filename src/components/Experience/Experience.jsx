addfc651a8fae00ddb2b5cc09161a2a8cb8bac77
Hitesh <soneta.h@northeastern.edu>
2024-01-04 20:57:13 -0500

    contact page responsive

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -13,11 +13,12 @@ export const Experience = () => {
           {history.map((historyItem, id) => {
             return (
               <li key={id} className={styles.historyItem}>
+                
+                <div className={styles.historyItemDetails}>
                 <img
                   src={historyItem.imageSrc}
                   alt={`${historyItem.organisation} Logo`}
                 />
-                <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                   <ul>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -66,7 +66,7 @@ export const Navbar = () => {
             <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="#about">Contact</a>
+            <a href="#contact">Contact</a>
           </li>
           <li>
             <a href="#" onClick={downloadResume}>Resume</a>
